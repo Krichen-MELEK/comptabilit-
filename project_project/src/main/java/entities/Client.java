dd3863c3f994df76ae9611655c196b4ba49f87c1
Yassin <yassin.makni.20@gmail.com>
2018-08-11 13:55:46 +0100

    update client

--- project_project/src/main/java/entities/Client.java
+++ project_project/src/main/java/entities/Client.java
@@ -1,5 +1,25 @@
 package entities;
 
-public class Client {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Client extends Personne {
+
+    String nomSociete ;
+    String matricule ;
+    String telSociete ;
+    String numRegistreCommerce ;
+    String secteurActivite ;
+    String adresseSociete ;
+    String emailSociete ;
 
 }
