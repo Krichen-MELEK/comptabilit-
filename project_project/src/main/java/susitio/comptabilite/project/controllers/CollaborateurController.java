718c530fe609c4701e671fecc40b09bb8e021f45
Yassin <yassin.makni.20@gmail.com>
2018-08-12 23:36:46 +0100

    impl controller

---
+++ project_project/src/main/java/susitio/comptabilite/project/controllers/CollaborateurController.java
@@ -0,0 +1,9 @@
+package susitio.comptabilite.project.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/collaborateur")
+public class CollaborateurController {
+}
