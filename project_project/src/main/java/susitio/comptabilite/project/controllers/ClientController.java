2a64575139ea918c4bf3fe16bf58e92dbcdc1426
KRICHEN Melek <krichen0melek@gmail.com>
2018-08-25 16:25:07 +0200

    fix some problem

--- project_project/src/main/java/susitio/comptabilite/project/controllers/ClientController.java
+++ project_project/src/main/java/susitio/comptabilite/project/controllers/ClientController.java
@@ -1,19 +1,13 @@
 package susitio.comptabilite.project.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-=======
-import org.springframework.stereotype.Controller;
->>>>>>> origin/master
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-<<<<<<< HEAD
 import susitio.comptabilite.project.entities.Client;
 import susitio.comptabilite.project.services.ClientService;
-
 
 @RestController
 @RequestMapping("/client")
@@ -27,16 +21,6 @@ public class ClientController {
 		clientService.addClient(client);
 		return client;
 	}
-=======
-import susitio.comptabilite.project.services.DocumentService;
+    
+}   
 
-@Controller
-@RequestMapping("/client")
-public class ClientController {
-	@Autowired
-	DocumentService documentService;
-	
-
-	
->>>>>>> origin/master
-}
